9722fad90e45c9767d982f050957cccead4ca610
gauravgupta-eclair <33214306+gauravgupta-eclair@users.noreply.github.com>
2018-02-21 17:06:31 +1100

    changes

---
+++ src/sample_code.py
@@ -0,0 +1,7 @@
+
+class Sample:
+    x =0
+    def inc(self,x):
+        x = x+ 1
+        print(x)
+        return x
---
+++ test/test.py
@@ -0,0 +1,6 @@
+from src.sample_code import Sample
+
+def test_answer():
+    s = Sample()
+    a =4
+    assert s.inc(a) == 5
